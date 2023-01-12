ad049676c05e8f7011ea53435ea66f1ac6258802
RaimondoCosma <raimondo.cosma@gmail.com>
2023-01-12 16:04:13 +0100

    Aggiunge pagina Welcome

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Primi Passi</title>
+    <link rel="stylesheet" href="../css/app.css">
+    <style>
+        /* ---------------------
+            RESET
+        --------------------- */
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        /* ---------------------
+            COMMON
+        --------------------- */
+        body {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 100vw;
+            height: 100vh;
+            background-image: linear-gradient(to right top, #030b16, #212041, #5b265f, #9f1c63, #da1d4b);
+        }
+        h1 {
+            font-size: 40px;
+            padding: 40px 60px;
+            color: #da1d4b;
+            background-color: white;
+            border-radius: 20px;
+            box-shadow: 5px 5px 10px 0px #000000;
+        }   
+    </style>
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>
